219fb4baa193f0d46f01ec5a18906f7ddecc78d9
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-20 18:19:25 +0300

    fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,9 @@
 def fibonacci_sequence(n):
     if n == 0:
-        return [0]
+        return []
     elif n == 1:
+        return [0]
+    elif n == 2:
         return [0, 1]
     else:
         lst = fibonacci_sequence(n-1)
