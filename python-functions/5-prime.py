8f196f67ff35aba179160a0adf382567d99c128c
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-20 20:23:12 +0300

    prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,10 @@
 def is_prime(number):
     if number == 1:
         return True
+    elif number == 0:
+        return False
+    elif number < 0:
+        number = number * -1
     for i in range(1, number):
         if number % i == 0:
             return True
