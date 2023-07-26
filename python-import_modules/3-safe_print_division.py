57c2541c29bb936c337575b0b14c6888670360ab
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-27 02:53:06 +0300

    Add safe print division task

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        if isinstance(a, int) and isinstance(b, int):
+            result = a / b
+    except:
+        pass
+
+    finally:
+        print("Inside result: ".format(result))
+        return result
