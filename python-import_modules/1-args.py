230a3b870536ef4419351e6ddf1b7863249ac02b
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-27 02:19:02 +0300

    Add args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    print("{} argument{}".format(len(argv)), end="")
+    if len(argv) != 1:
+        print("s", end="")
+        if len(argv) == 0:
+            print(".")
+        else:
+            print(":")
+
+    for i in range(1, len(argv)+1):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
