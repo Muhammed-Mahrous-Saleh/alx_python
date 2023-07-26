d77fbdb7daa8440a52be44b77b1cae23da3d3cb5
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-27 02:53:59 +0300

    Add safe print division task

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
         pass
 
     finally:
-        print("Inside result: ".format(result))
+        print("Inside result: {}".format(result))
         return result
