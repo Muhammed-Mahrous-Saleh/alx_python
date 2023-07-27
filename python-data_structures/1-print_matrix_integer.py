a99cedcdf2b743ae9984c69e3431fba9f3b039f2
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-28 02:21:33 +0300

    print matrix integer task

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -5,8 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         row_print = ""
         for number in row:
             if row_print == "":
-                row_print = "{}".format(number)
+                row_print = "{:d}".format(number)
             else:
-                row_print = row_print + " {}".format(number)
+                row_print = row_print + " {:d}".format(number)
 
         print(row_print)
