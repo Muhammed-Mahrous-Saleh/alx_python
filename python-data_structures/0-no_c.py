c81244663d12552835cc16f9092085dfff92435c
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-28 02:04:28 +0300

    on c task

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 def no_c(text):
-    text = text.replace("C", "")
-    text = text.replace("c", "")
-    return text
+    my_text = []
+    for i in range(len(text)):
+        if text[i] in ["c", "C"]:
+            my_text.append('')
+        else:
+            my_text.append(text[i])
+    return ''.join(my_text)
