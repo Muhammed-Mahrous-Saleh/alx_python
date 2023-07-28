827bafd06496f47cf2fe24596a54381f40ceb7b0
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-28 22:45:36 +0300

    Best score

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    no_best = None
+    if a_dictionary:
+        max_value = max(list(a_dictionary.values()))
+        for key in a_dictionary.keys():
+            if a_dictionary[key] == max_value:
+                return key
+    return no_best
