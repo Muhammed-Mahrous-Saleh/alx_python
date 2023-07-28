7dfdf614ef1aff548055c7cb4f9a26965f756853
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-29 00:45:38 +0300

    update

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    def multiply(num1):
-        return num1*number
-    return list(map(multiply, my_list))
+    return list(map(lambda n: n*number, my_list))
