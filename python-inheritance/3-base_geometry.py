d2471e1c3b3751c0532eb4b01ae8f7ff74904299
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-29 17:30:59 +0300

    3. Geometry module task

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Defines an empty class BaseGeometry"""
-
-
-def __dir__(self):
-    return [attr for attr in dir(BaseGeometry) if attr != '__init_subclass__']
-
-
-class BaseGeometry(object):
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
+class BaseGeometry(metaclass=NoInitSubclassMeta):
     """Empty class
     """
-    pass
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
