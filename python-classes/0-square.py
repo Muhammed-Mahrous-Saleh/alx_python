f6437aab569068e55e8b8e19685822a00b44180d
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-29 12:16:21 +0300

    add square class task

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 class Square:
+    """Square Class"""
     def __init__(self, size):
         self.set_size(size)
 
     def set_size(self, size):
+        """set a size value of the Square object"""
         self.__size = size
     
