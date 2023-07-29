7ae9d0c8db1ddf779fe1c8490e0017c7e4afffcc
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-29 14:04:24 +0300

    add square class task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,11 +9,15 @@ class Square:
     Attributes:
         size: size of a square (1 side).
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Creates new instances of square (1 side).
 
         Args:
             size: size of the square.
         """
-        self.__size = size
-        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
