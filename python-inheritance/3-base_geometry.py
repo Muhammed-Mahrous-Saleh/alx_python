06d96a2d226e0609afe4f50892b86115f904e806
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-29 17:01:40 +0300

    3. Geometry module task

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """Defines an empty class BaseGeometry"""
-import sys
+
 
 class BaseGeometry:
     """Empty class
     """
-    
-    print("Python version")
-    print (sys.version)
-
-    print("Version info.")
-    print (sys.version_info)
     pass
