deb9b7e7c4a7a58133917399b94fb08e87b2e757
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-08-07 22:08:27 +0300

    Add python network project

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print('Body response:')
         print('\t- type: {}'.format(type(r.text)))
         print('\t- content: {}'.format(r.text))
-        print('\t- content2: {}'.format(r))
+        print('\t- content2: {}'.format(r.content))
