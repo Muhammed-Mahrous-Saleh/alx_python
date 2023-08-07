ea71699c3e9556a50b4887b5d024da24bcab714d
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-08-07 23:02:31 +0300

    fix issues

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
