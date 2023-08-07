2d6818676c87c7733d5c270587e8177fd82d351f
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-08-07 20:43:04 +0300

    Create Square Class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"Define Square class inherits Rectangle class"
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    """Class that defines properties of Square.
+
+    Attributes:
+        size (int): length of a single line.
+        x (int): x.
+        y (int): y.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Creates new instances of Square
+
+        Args:
+            size (int): width and height of square.
+            x (int, optional): x. Defaults to 0.
+            y (int, optional): y. Defaults to 0.
+            id (int, optional): Identity number of square. Defaults to None.
+        """
+        super().__init__(id, size, size, x, y)
+
+    def __str__(self):
+        """Overriding the method 
+
+        Returns:
+            str: [Square] (<id>) <x>/<y> - <size>"""
+
+        return f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__width}"
