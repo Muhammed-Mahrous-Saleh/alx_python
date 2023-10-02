38c73a2c2159b90df0c36375e94a868eaf1b4fbb
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-10-02 23:17:56 +0300

    upload tasks

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,7 +2,19 @@ import requests
 import sys
 import json
 
+
 def export_todo_to_json(employee_id):
+    """
+    Fetches employee details and their TODO list from a provided API.
+    Writes the TODO list in a JSON format to a file named after the employee's ID.
+
+    Args:
+    - employee_id (int): The ID of the employee for whom the TODO list needs to be fetched.
+
+    Returns:
+    None
+    """
+
     # Get employee details
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     user_response = requests.get(user_url)
@@ -27,11 +39,14 @@ def export_todo_to_json(employee_id):
     with open(filename, 'w') as file:
         json.dump(data, file)
 
+
 if __name__ == "__main__":
+    # Ensure correct number of command line arguments
     if len(sys.argv) != 2:
         print("Usage: python3 script_name.py EMPLOYEE_ID")
         sys.exit(1)
-    
+
+    # Fetch and write the TODO list for the provided employee ID
     try:
         employee_id = int(sys.argv[1])
         export_todo_to_json(employee_id)
